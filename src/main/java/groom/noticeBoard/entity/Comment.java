b8b1d87d6affe215112c77c0b9d4afde03b860b5
koreaygj <ygj1828@gmail.com>
2023-12-29 12:13:00 +0900

    add:Comment data class

--- src/main/java/groom/noticeBoard/entity/Comment.java
+++ src/main/java/groom/noticeBoard/entity/Comment.java
@@ -8,17 +8,20 @@ public class Comment {
   private Long commentId;
   private Long postId;
   private Long userId;
+  private String username;
   private String content;
   private String commentDate;
   private String updateDate;
 
   private boolean isDeleted;
 
-  public Comment(Long commentId, Long postId, Long userId, String content, String commentDate,
+  public Comment(Long commentId, Long postId, Long userId, String username, String content,
+      String commentDate,
       boolean isDeleted) {
     this.commentId = commentId;
     this.postId = postId;
     this.userId = userId;
+    this.username = username;
     this.content = content;
     this.commentDate = commentDate;
     this.isDeleted = isDeleted;
