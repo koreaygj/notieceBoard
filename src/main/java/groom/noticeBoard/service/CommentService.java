df591171e2950cc4d8f878e9ea27b145e19c413d
koreaygj <ygj1828@gmail.com>
2023-12-29 12:13:30 +0900

    add:Comment service

---
+++ src/main/java/groom/noticeBoard/service/CommentService.java
@@ -0,0 +1,21 @@
+package groom.noticeBoard.service;
+
+import groom.noticeBoard.entity.Comment;
+import groom.noticeBoard.repository.CommentRepository;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+  CommentRepository commentRepository = CommentRepository.getCommentRepository();
+
+  public Comment registerComment(Comment comment) throws SQLException {
+    return commentRepository.save(comment);
+  }
+
+  public List<Comment> getComments(Long postId) throws SQLException {
+    return commentRepository.searchCommentByPostId(postId);
+  }
+}
